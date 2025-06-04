de8d977a9b5589f3702ae9c524876a550991a8ad
William Gruszka <40753609+wilgru@users.noreply.github.com>
2025-06-04 19:00:20 +1000

    tweaked tasks table

---
+++ pocketbase/pb_migrations/1749025814_updated_tasks.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text724990059",
+    "max": 0,
+    "min": 0,
+    "name": "title",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text724990059",
+    "max": 0,
+    "min": 0,
+    "name": "title",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
