4c517dc85f0d7408676711edc487f04272d482c2
William Gruszka <40753609+wilgru@users.noreply.github.com>
2025-07-12 11:02:03 +1000

    updated journals and related tables

---
+++ pocketbase/pb_migrations/1750975622_updated_journals.js
@@ -0,0 +1,46 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1407978968")
+
+  // remove field
+  collection.fields.removeById("relation18589324")
+
+  // remove field
+  collection.fields.removeById("relation1347970455")
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1407978968")
+
+  // add field
+  collection.fields.addAt(5, new Field({
+    "cascadeDelete": false,
+    "collectionId": "75h1r0vz3gvycqp",
+    "hidden": false,
+    "id": "relation18589324",
+    "maxSelect": 999,
+    "minSelect": 0,
+    "name": "notes",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_2602490748",
+    "hidden": false,
+    "id": "relation1347970455",
+    "maxSelect": 999,
+    "minSelect": 0,
+    "name": "tasks",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+})
