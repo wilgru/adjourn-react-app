af3e10b718e97b32f5d432f756b4b85542712081
William Gruszka <40753609+wilgru@users.noreply.github.com>
2025-10-10 19:47:00 +1100

    updated ID generation characters

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,7 +1,6 @@
 export const generateId = (length: number = 15) => {
   let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
