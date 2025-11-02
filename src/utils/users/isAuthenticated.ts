f559f5191e5cd20a491e3d3957d450d0cf12c376
William Gruszka <40753609+wilgru@users.noreply.github.com>
2025-11-02 21:33:18 +1100

    more accurate check for user authentication

--- src/utils/users/isAuthenticated.ts
+++ src/utils/users/isAuthenticated.ts
@@ -1,5 +1,11 @@
 import { pb } from "src/connections/pocketbase";
 
 export default function isAuthenticated() {
-  return !!pb.authStore?.record;
+  const isAuthenticated = pb.authStore?.isValid; // currently valid for 2 weeks
+
+  if (!isAuthenticated) {
+    pb.authStore.clear();
+  }
+
+  return isAuthenticated;
 }
