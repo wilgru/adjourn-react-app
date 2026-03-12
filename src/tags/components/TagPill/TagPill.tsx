a3e2caad0b60e62707e715035c2a2829a0f76a55
William Gruszka <40753609+wilgru@users.noreply.github.com>
2026-03-13 07:23:18 +1100

    made tag pill not a button if theres no onClick

--- src/tags/components/TagPill/TagPill.tsx
+++ src/tags/components/TagPill/TagPill.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "src/common/components/Button/Button";
 import { cn } from "src/common/utils/cn";
+import { Icon } from "src/icons/components/Icon/Icon";
 import type { Tag } from "src/tags/Tag.type";
 
 type TagPillProps = {
@@ -21,6 +22,9 @@ export const TagPill = ({
   onClick,
 }: TagPillProps): JSX.Element => {
   const [closeButtonVisible, setCloseButtonVisible] = useState<boolean>(false);
+  const iconName = closable && closeButtonVisible ? "x" : tag.icon;
+  const isClickable = Boolean(onClick);
+  const handleClick = onClick ? () => onClick(tag.id) : undefined;
 
   return (
     <div
@@ -28,24 +32,53 @@ export const TagPill = ({
       onMouseOver={() => setCloseButtonVisible(true)}
       onMouseOut={() => setCloseButtonVisible(false)}
     >
-      <Button
-        variant={variant}
-        colour={tag.colour}
-        size={size}
-        onClick={() => onClick && onClick(tag.id)}
-        disabled={!onClick}
-        iconName={closable && closeButtonVisible ? "x" : tag.icon}
-      >
-        {!collapsed && (
-          <span
-            className={cn(
-              "inline-block overflow-hidden transition-all duration-300 ease-in-out max-w-[10rem] opacity-100",
-            )}
-          >
-            {tag.name}
-          </span>
-        )}
-      </Button>
+      {isClickable ? (
+        <Button
+          variant={variant}
+          colour={tag.colour}
+          size={size}
+          onClick={handleClick}
+          iconName={iconName}
+        >
+          {!collapsed && (
+            <span
+              className={cn(
+                "inline-block overflow-hidden transition-all duration-300 ease-in-out max-w-[10rem] opacity-100",
+              )}
+            >
+              {tag.name}
+            </span>
+          )}
+        </Button>
+      ) : (
+        // TODO: refactor to remove duplicate styling code between this and the Button version
+        <div
+          className={cn(
+            "h-fit w-fit flex items-center gap-2 rounded-full transition-colors",
+            size === "xs" && "text-[0.625rem] font-normal py-0.5 pl-0.5 pr-1",
+            size === "sm" && "text-xs font-normal py-1 pl-1 pr-2",
+            size === "md" && "text-sm font-medium py-2 pl-2 pr-3",
+            size === "lg" && "text-md font-medium py-3 pl-3 pr-4",
+            variant === "block" && tag.colour.textPill,
+            variant === "block" && tag.colour.backgroundPill,
+            variant === "block" && tag.colour.textPillInverted,
+            variant === "block" && tag.colour.backgroundPillInverted,
+            variant === "ghost" && "text-slate-500",
+            variant === "link" && "underline-offset-4",
+          )}
+        >
+          <Icon iconName={iconName} size={size} weight="regular" />
+          {!collapsed && (
+            <span
+              className={cn(
+                "inline-block overflow-hidden transition-all duration-300 ease-in-out max-w-[10rem] opacity-100",
+              )}
+            >
+              {tag.name}
+            </span>
+          )}
+        </div>
+      )}
     </div>
   );
 };
